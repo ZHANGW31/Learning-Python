bf61160101b3426a4dd7b04e40534650d72f7cb2
Wei Zhang <wei.zhang4@outlook.com>
2020-04-21 10:23:15 -0700

    Added demonstration of utilizing the input method

--- .vscode/hello.py
+++ .vscode/hello.py
@@ -1,3 +1,12 @@
+
 msg = "Hello World!" 
 
 print(msg)
+
+#Python uses the # symbol to signify comments.
+
+x = 2;
+
+y = x + 5;
+
+print(y)
---
+++ .vscode/inputMethod.py
@@ -0,0 +1,6 @@
+print("Please enter a small int: ");
+smallInt = input(); #The input() method here allows for the user to provide an input
+print("small int: ")
+print(smallInt);
+
+
