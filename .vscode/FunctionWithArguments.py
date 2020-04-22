667acad445f21403f5070559568e0d4d8c559cf4
Wei Zhang <wei.zhang4@outlook.com>
2020-04-21 18:30:37 -0700

    Added example and demonstration of the print function

---
+++ .vscode/FunctionWithArguments.py
@@ -0,0 +1,14 @@
+#The print function can take an infinite amount of parameters
+
+#This prints Hello World! I am sending string arguments to print, the comma combines the two strings.
+
+#Note that the comma that seperates the two strings adds a space.
+print('Hello World!','I am sending string arguments to print')
+
+student_age = 25
+student_name = "Wei Zhang"
+print(student_name,'will be in the class for',student_age,'year old students.')
+#This prints out: Wei Zhang will be in the class for 25 year old students.
+
+
+
