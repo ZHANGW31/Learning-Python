81a5ce17275befb57245998e6054ba6d50669cfb
Wei Zhang <wei.zhang4@outlook.com>
2020-04-22 20:31:05 -0700

    Added requirements for end of mod2 assignment

---
+++ .vscode/Mod2.py
@@ -0,0 +1,16 @@
+#This program requires a function be defined , created , and called. The call will send values based on user input. The function call must capture a return value that is used
+#in the print output. The function will have parameters and return a string and should otherwise use code syntax covered in module 2.
+
+# This is the end of module coding assignment.
+
+#Program fishstore()
+
+#Create and test fishstore()
+
+#fishstore() takes 2 string arguments: fish and price
+
+#fishtore returns a string in sentence form
+
+#gather input for fish_entry and price_entry to use in calling fishstore()
+
+#print the return value of fishstore()
