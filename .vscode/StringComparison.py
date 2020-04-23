c43bc6d746fd25d4ce9e0946319f1ace347585c7
Wei Zhang <wei.zhang4@outlook.com>
2020-04-23 09:02:59 -0700

    Added string comparison examples.

---
+++ .vscode/StringComparison.py
@@ -0,0 +1,22 @@
+#Strings can be equal == or unequal !=
+#Strings can be greater than > or less than <
+#alphabetically "A" is less than "B"
+#lower case "a" is greater than upper case "A"
+
+#This returns false
+hello_comparison = "hello" < "Hello"
+
+print(hello_comparison)
+
+#This returns false
+zebra_comparison = "Aardvark" > "Zebra"
+
+print(zebra_comparison)
+
+#This returns True
+student_comparison = 'student' != 'Student'
+
+print(student_comparison)
+
+print("'student' >= 'Student' is", 'student' >= 'Student')
+print("'student' != 'Student' is", 'student' != 'Student')
